9907f09398eee6def3a8e1a875e16069fd31e535
HUSEYIN OZDEMIR <78041148+HsynHsyn@users.noreply.github.com>
2024-05-11 10:15:12 +0100

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,21 @@
+package Day7_If_Statements;
+
+
+public class Task2 {
+	
+	public static void main(String[] args) {
+		
+		int score = 80;
+		
+		 if (score >=70) {
+			 System.out.println("Excelent");
+			 System.out.println("You passed with grade A");
+
+			 
+		 }
+		 
+		 System.out.println("*************************");
+	}
+
+
+}
